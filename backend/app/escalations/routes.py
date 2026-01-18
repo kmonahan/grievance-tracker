50854c08bcb49040cfc805c93fa407dbeb3e37a6
KJ Monahan <kejm42@gmail.com>
2026-01-18 17:29:00 -0600

    Initial work to get recent activity

--- backend/app/escalations/routes.py
+++ backend/app/escalations/routes.py
@@ -13,10 +13,14 @@ def _convert_to_date(date_as_string):
 
 @bp.route('/edit/<int:escalation_id>', methods=['POST'])
 def edit_escalation(escalation_id):
-    escalation = Escalation.query.get_or_404(escalation_id)
+    escalation = db.session.execute(db.select(Escalation)).get_or_404(escalation_id)
     date_due = request.form.get('date_due', type=_convert_to_date)
     if date_due is None:
         return jsonify({'error': 'Missing or invalid due date'}), 400
     escalation.date_due = date_due
     db.session.commit()
     return jsonify({'ok': True}), 200
+
+@bp.route('/recent', methods=['GET'])
+def get_recent_escalations():
+    escalations = db.session.execute(db.select(Escalation).filter().order_by(Escalation.date_due.desc())).scalars()
