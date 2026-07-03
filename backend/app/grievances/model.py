31c2b328cd404bdfa10ab81e9820b212613bed3c
KJ Monahan <kejm42@gmail.com>
2026-07-03 08:36:41 -0500

    Update database and model to store secondary person

--- backend/app/grievances/model.py
+++ backend/app/grievances/model.py
@@ -8,6 +8,8 @@ class Grievance(db.Model):
     category = db.relationship('Category', back_populates='grievances')
     point_person_id = db.Column(db.Integer, db.ForeignKey('user.id'), unique=False, nullable=True)
     point_person = db.relationship('User', back_populates='grievances')
+    secondary_id = db.Column(db.Integer, db.ForeignKey('user.id'), unique=False, nullable=True)
+    secondary = db.relationship('User', back_populates='secondary_grievances')
     escalations = db.relationship('Escalation', back_populates='grievance', cascade='all, delete')
 
     def to_dict(self):
@@ -16,5 +18,7 @@ class Grievance(db.Model):
         del grievance_dict['category_id']
         grievance_dict['point_person'] = self.point_person.name if self.point_person else None
         del grievance_dict['point_person_id']
+        grievance_dict['secondary'] = self.secondary.name if self.secondary else None
+        del grievance_dict['secondary_id']
         grievance_dict['escalations'] = [escalation.to_dict() for escalation in self.escalations if self.escalations is not None]
         return grievance_dict
